563426fca1f30327c9fb22694531c0d7bd70f648
Suresh-75 <suresh710k@gmail.com>
2025-05-20 15:47:54 +0530

    deployV1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/orbit/",
   resolve: {
     alias: {
       "@": path.resolve(path.dirname(import.meta.url), "src"), // Define alias for "src" folder
