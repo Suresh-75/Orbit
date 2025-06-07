7a2b2cc4f54889cd0630b62c922937ce188ddeeb
Suresh-75 <suresh710k@gmail.com>
2025-06-08 00:12:35 +0530

    sponsors changes

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -4,11 +4,11 @@ import sp3 from "./assets/sponsor3.webp";
 import sp2 from "./assets/sp2.png";
 function Sponsors() {
   return (
-    <div className=" flex justify-center h-[25vh] my-25 max-lg:mt-10 max-lg:mb-4 flex-col">
+    <div className=" flex justify-center h-[100%] my-25 max-lg:mt-10 max-lg:mb-6 flex-col">
       <h2 className="text-center text-6xl  font-bold bg-gradient-to-r from-blue-200 to-blue-400 bg-clip-text text-transparent  max-lg:text-4xl max-lg:mb-5">
         SPONSORS
       </h2>
-      <div className=" flex justify-center items-center">
+      <div className=" flex justify-center items-center flex-wrap max-lg:flex-col">
         <a target="_blank" href="https://www.simscale.com/">
           <img
             src={sp1}
@@ -30,7 +30,7 @@ function Sponsors() {
           <img
             src={sp3}
             alt="sp3"
-            className="h-30 ml-10 max-lg:h-25 max-lg:m-0"
+            className="h-30 ml-10 max-lg:h-18 max-lg:m-0"
           />
         </a>
       </div>
